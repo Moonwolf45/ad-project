a3db8aea36a3925d7e425480cd2480f2c39b76c3
Vladislav <miznikov.1995@mail.ru>
2019-06-06 01:15:00 +0500

    Установка и настройка Vue

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import './plugins/vuetify'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
