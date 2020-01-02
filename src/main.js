57736c01ee9b90fb52c569d00544992b6b2623e0
Vladislav <miznikov.1995@mail.ru>
2020-01-02 20:05:25 +0500

    Настройка firebase

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App'
 import router from './router'
 import store from './store'
 import Vuetify from 'vuetify'
+import * as firebase from 'firebase'
 import 'vuetify/dist/vuetify.min.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
@@ -16,5 +17,16 @@ new Vue({
   router,
   store,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAK2nGJvmz5y9wuNH-8sp8wp84R3n7p3Pk',
+      authDomain: 'itc-ads-367ec.firebaseapp.com',
+      databaseURL: 'https://itc-ads-367ec.firebaseio.com',
+      projectId: 'itc-ads-367ec',
+      storageBucket: 'itc-ads-367ec.appspot.com',
+      messagingSenderId: '579834797424',
+      appId: '1:579834797424:web:c8030ffb76714a95b6c3b5'
+    })
+  }
 })
