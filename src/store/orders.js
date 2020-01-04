179422be5ace182ace829065336898f7cf3540a3
Vladislav <miznikov.1995@mail.ru>
2020-01-04 21:44:30 +0500

    Создание и обработка заказов

--- src/store/orders.js
+++ src/store/orders.js
@@ -1,20 +1,85 @@
+import firebase from 'firebase/app'
+import 'firebase/database'
+
+class Order {
+  constructor (name, phone, adId, done = false, id = null) {
+    this.name = name
+    this.phone = phone
+    this.adId = adId
+    this.done = done
+    this.id = id
+  }
+}
+
 export default {
   state: {
     orders: []
   },
   mutations: {
-    createOrder (state, payload) {
-      state.orders.push(payload)
+    loadOrders (state, payload) {
+      state.orders = payload
     }
   },
   actions: {
-    async createOrder () {
-      await new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve()
-        }, 4000)
-      })
+    async createOrder ({commit}, {name, phone, adId, ownerId}) {
+      const order = new Order(name, phone, adId)
+      commit('clearError')
+
+      try {
+        await firebase.database().ref(`/users/${ownerId}/orders`).push(order)
+      } catch (error) {
+        commit('setError', error.message)
+        throw error
+      }
+    },
+    async fetchOrders ({commit, getters}) {
+      commit('clearError')
+      commit('setLoading', true)
+      const resultOrders = []
+
+      try {
+        const fbVal = await firebase.database().ref(`/users/${getters.user.id}/orders`).once('value')
+        const orders = fbVal.val()
+
+        if (orders.length !== 0) {
+          Object.keys(orders).forEach(key => {
+            const order = orders[key]
+            resultOrders.push(
+              new Order(order.name, order.phone, order.adId, order.done, key)
+            )
+          })
+        }
+
+        commit('loadOrders', resultOrders)
+        commit('setLoading', false)
+      } catch (error) {
+        commit('setError', error.message)
+        commit('setLoading', false)
+        throw error
+      }
+    },
+    async markOrderDone ({commit}, payload) {
+      commit('clearError')
+      try {
+        await firebase.database().ref(`/users/${getters.user.id}/orders`).child(payload).update({
+          done: true
+        })
+      } catch (error) {
+        commit('setError', error.message)
+        throw error
+      }
+    }
     }
   },
-  getters: {}
+  getters: {
+    doneOrders (state) {
+      return state.orders.filter(order => order.done)
+    },
+    unDoneOrders (state) {
+      return state.orders.filter(order => !order.done)
+    },
+    orders (state, getters) {
+      return getters.unDoneOrders.concat(getters.doneOrders)
+    }
+  }
 }
